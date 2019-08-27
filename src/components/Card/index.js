0dbc4149dd81c4d426cfdb339b9da496758a2091
Charles Munat <Charles_Munat@bnz.co.nz>
2019-08-28 09:37:40 +1200

    Make responsive

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 const Card = styled.div`
   border: 1px solid gray;
+  border-radius: 10px;
   height: 22rem;
+  margin: 0 auto 1rem;
   max-width: 100%;
   width: 16rem;
 `
